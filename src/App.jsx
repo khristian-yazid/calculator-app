74480c029f1396b2948f4a9f9de7aa58c28fac08
Khristian Ali <khristian-yazid1@hotmail.com>
2021-08-04 16:59:22 -0400

    apps comopents finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import React from "react";
-import Button from "./components/Button";
+import React, { useState } from "react";
 import MathOperations from "./components/MathOperations";
+import Numbers from "./components/Numbers";
+import Functions from "./components/Functions";
 import Result from "./components/Result";
-import "./App.css";
+
+import "./assets/styles/App.css";
 
 // Función Flecha o Arrow Function
 const App = () => {
   // Lo que ejecuta la función
-  const clickHandlerFunction = (text) => {
-    console.log("button.clickHandler", text);
-  };
+  const [stack, setStack] = useState("");
+
   return (
     <main className="react-calculator">
-      <Result value={undefined} />
-      <div className="numbers">
-        <Button text="1" clickHandler={clickHandlerFunction} />
-        <Button text="2" clickHandler={clickHandlerFunction} />
-        <Button text="3" clickHandler={clickHandlerFunction} />
-        <Button text="4" clickHandler={clickHandlerFunction} />
-        <Button text="5" clickHandler={clickHandlerFunction} />
-        <Button text="6" clickHandler={clickHandlerFunction} />
-        <Button text="7" clickHandler={clickHandlerFunction} />
-        <Button text="8" clickHandler={clickHandlerFunction} />
-        <Button text="9" clickHandler={clickHandlerFunction} />
-        <Button text="0" clickHandler={clickHandlerFunction} />
-      </div>
-      <div className="functions">
-        <button>clear</button>
-        <button>r</button>
-      </div>
+      <Result value={stack} />
+      <Numbers
+        onClickNumber={(number) => {
+          console.log("Numbers", number);
+          setStack(number);
+        }}
+      />
+      <Functions
+        onContentClear={(funcion) => console.log("Funcion", funcion)}
+        onDelete={(funcion) => console.log("delete", funcion)}
+      />
       <MathOperations
         onClickOperation={(operation) => console.log("operation: ", operation)}
         onClickEqual={(equal) => console.log("Equeal:", equal)}
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import '../assets/styles/components/Button.css'
 
 const Button = ({ type, text, clickHandler }) => (
-  <button className={type} onClick={() => clickHandler(text)}>
-    <span>{text}</span>
-  </button>
+    <button className={type} onClick={() => clickHandler(text)}>
+        <span>{text}</span>
+    </button>
 );
 
 Button.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string.isRequired,
-  clickHandler: PropTypes.func.isRequired,
+    type: PropTypes.string,
+    text: PropTypes.string.isRequired,
+    clickHandler: PropTypes.func.isRequired,
 };
 
 export default Button;
